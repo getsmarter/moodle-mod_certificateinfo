1a4abca8d746ad21b71226321db605c5bc06d68c
Andrew Tainton <andrew@getsmarter.co.za>
2016-08-10 13:48:35 +0200

    update the plugin name to be certificateinfo

--- lang/en/certificateinfo.php
+++ lang/en/certificateinfo.php
@@ -16,27 +16,27 @@
 
 
 /**
- * English strings for newmodule
+ * English strings for certificateinfo
  *
  * You can have a rather longer description of the file as well,
  * if you like, and it can span multiple lines.
  *
- * @package    mod_newmodule
+ * @package    mod_certificateinfo
  * @copyright  2016 Your Name <your@email.address>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['modulename'] = 'newmodule';
-$string['modulenameplural'] = 'newmodules';
-$string['modulename_help'] = 'Use the newmodule module for... | The newmodule module allows...';
-$string['newmodule:addinstance'] = 'Add a new newmodule';
-$string['newmodule:submit'] = 'Submit newmodule';
-$string['newmodule:view'] = 'View newmodule';
-$string['newmodulefieldset'] = 'Custom example fieldset';
-$string['newmodulename'] = 'newmodule name';
-$string['newmodulename_help'] = 'This is the content of the help tooltip associated with the newmodulename field. Markdown syntax is supported.';
-$string['newmodule'] = 'newmodule';
-$string['pluginadministration'] = 'newmodule administration';
-$string['pluginname'] = 'newmodule';
+$string['modulename'] = 'certificateinfo';
+$string['modulenameplural'] = 'certificateinfos';
+$string['modulename_help'] = 'Use the certificateinfo module for... | The certificateinfo module allows...';
+$string['certificateinfo:addinstance'] = 'Add a new certificateinfo';
+$string['certificateinfo:submit'] = 'Submit certificateinfo';
+$string['certificateinfo:view'] = 'View certificateinfo';
+$string['certificateinfofieldset'] = 'Custom example fieldset';
+$string['certificateinfoname'] = 'certificateinfo name';
+$string['certificateinfoname_help'] = 'This is the content of the help tooltip associated with the certificateinfoname field. Markdown syntax is supported.';
+$string['certificateinfo'] = 'certificateinfo';
+$string['pluginadministration'] = 'certificateinfo administration';
+$string['pluginname'] = 'certificateinfo';
